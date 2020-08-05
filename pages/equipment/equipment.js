eaeed4cb66665e3780c8c4a2e2af2004701310ea
sunfeifeifei <1501271800@qq.com>
2020-08-05 17:09:51 +0800

    20200805

--- pages/equipment/equipment.js
+++ pages/equipment/equipment.js
@@ -5,6 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    showModalStatus: false,
     list:[
       {
         number:'52857485',
@@ -24,10 +25,44 @@ Page({
       }
     ],
   },
+  
 
-  handleSetter(e){
-    console.log(e.currentTarget.dataset.item);
+  handleSetter(e) {
+    console.log('--当前点击--', e.currentTarget.dataset.item);
+    this.util(this, 'open');
+  },
 
+  powerDrawer() {
+    this.util(this, 'close');
+  },
+
+  //模态框动画
+  util(that, currentStatu) {
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "ease-in-out",
+      delay: 0
+    });
+    animation.opacity(0).scale(0).step();
+    that.setData({
+      animationData: animation.export()
+    })
+    setTimeout(function () {
+      animation.opacity(1).scale(1).step();
+      that.setData({
+        animationData: animation
+      })
+      if (currentStatu == "close") {
+        that.setData({
+          showModalStatus: false
+        });
+      }
+    }.bind(that), 200)
+    if (currentStatu == "open") {
+      that.setData({
+        showModalStatus: true
+      });
+    }
   },
 
   /**
